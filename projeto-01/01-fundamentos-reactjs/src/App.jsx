e25dbdbe2f40c23a521256d0a6f9f0c3fd85bf59
Felipe Mamede <felipebrasilmv@gmail.com>
2022-07-08 19:20:06 -0300

    feat: iniciando com React

---
+++ projeto-01/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,11 @@
+import { Post } from './components/Post/Post'
+
+export function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Post author="Anne Doe" content="Lorem ipsum dolor sit amet, consectetur adip"/>
+      <Post author="Anne Doe2" content="Lorem ipsum dolor sit amet, consectetur adip sit amet, consectetur"/>
+    </>
+  )
+}
