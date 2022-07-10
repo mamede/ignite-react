a62d9b8f0ca3ba1bd62e2ff84582640351745391
Felipe Mamede <felipebrasilmv@gmail.com>
2022-07-09 22:29:49 -0300

    feat: estilização do post

--- projeto-01/01-fundamentos-reactjs/src/App.jsx
+++ projeto-01/01-fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,7 @@
-import { Header } from './components/Header/Header'
-import { Sidebar } from './components/Sidebar/Sidebar'
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 
-import { Post } from './components/Post/Post'
+import { Post } from './components/Post'
 
 import styles from './App.module.css';
 
--- projeto-01/01-fundamentos-reactjs/src/components/Header.jsx
+++ projeto-01/01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './Header.module.css';
 
-import igniteLogo from '../../assets/ignite-logo.svg';
+import igniteLogo from '../assets/ignite-logo.svg';
 
 export function Header() {
   return (
