697e44cf0c01e12773c2bab1ddddfbb06594dac4
Felipe Mamede <felipebrasilmv@gmail.com>
2022-07-09 22:07:03 -0300

    feat: finalizando sidebar

--- projeto-01/01-fundamentos-reactjs/src/App.jsx
+++ projeto-01/01-fundamentos-reactjs/src/App.jsx
@@ -1,11 +1,23 @@
+import { Header } from './components/Header/Header'
+import { Sidebar } from './components/Sidebar/Sidebar'
+
 import { Post } from './components/Post/Post'
+
+import styles from './App.module.css';
+
+import './global.css'
 
 export function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <Post author="Anne Doe" content="Lorem ipsum dolor sit amet, consectetur adip"/>
-      <Post author="Anne Doe2" content="Lorem ipsum dolor sit amet, consectetur adip sit amet, consectetur"/>
-    </>
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Anne Doe" content="Lorem ipsum dolor sit amet, consectetur adip"/>
+          <Post author="Anne Doe2" content="Lorem ipsum dolor sit amet, consectetur adip sit amet, consectetur"/>
+        </main>
+      </div>
+    </div>
   )
 }
---
+++ projeto-01/01-fundamentos-reactjs/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,25 @@
+import { PencilLine } from 'phosphor-react'
+import styles from './Sidebar.module.css';
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      />
+
+      <div className={styles.profile}>
+        <img src="https://github.com/mamede.png" className={styles.avatar} />
+        <strong>Felipe 'Campeiro' Mamede</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
