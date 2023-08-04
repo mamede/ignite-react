4759afb0a3facf9271d689c3a5fc947fb14afa29
Felipe Mamede <felipebrasilmv@gmail.com>
2023-08-04 11:04:30 -0300

    feat: tabela de transações

---
+++ projeto-03/dt-money/src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import logoImg from "../../assets/logo.svg";
+import * as S from "./styles";
+
+export function Header() {
+  return (
+    <S.HeaderContainer>
+      <S.HeaderContent>
+        <img src={logoImg} alt="" />
+
+        <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+      </S.HeaderContent>
+    </S.HeaderContainer>
+  );
+}
