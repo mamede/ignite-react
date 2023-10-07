d3d47b516a6c6fbdd203fd72226899c3ca9a7364
Felipe Mamede <felipebrasilmv@gmail.com>
2023-10-06 23:55:25 -0300

    feat: criando hook para o resumo

--- projeto-03/dt-money/src/components/Summary/index.tsx
+++ projeto-03/dt-money/src/components/Summary/index.tsx
@@ -1,31 +1,13 @@
-import { useContext } from "react";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
+
+import { useSummary } from "../../hooks/useSummary";
+
 import { priceFormatter } from "../../utils/formatter";
 
 import * as S from "./styles";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
-  console.log("transactions", transactions);
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
+  const summary = useSummary();
 
   return (
     <S.SummaryContainer>
