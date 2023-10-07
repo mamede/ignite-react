a53e6e5d98fb0b19cf1b7363d843ff9699dfb775
Felipe Mamede <felipebrasilmv@gmail.com>
2023-10-06 22:39:21 -0300

    feat criando contexto de transações

--- projeto-03/dt-money/src/components/NewTransactionModal/styles.ts
+++ projeto-03/dt-money/src/components/NewTransactionModal/styles.ts
@@ -1,6 +1,6 @@
 import styled, { css } from "styled-components";
-import * as Dialog from '@radix-ui/react-dialog';
-import * as RadioGroup from '@radix-ui/react-radio-group';
+import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -14,7 +14,7 @@ export const Content = styled(Dialog.Content)`
   min-width: 32rem;
   border-radius: 6px;
   padding: 2.5rem 3rem;
-  background: ${props => props.theme["gray-800"]};
+  background: ${(props) => props.theme["gray-800"]};
   position: fixed;
   top: 50%;
   left: 50%;
@@ -29,19 +29,19 @@ export const Content = styled(Dialog.Content)`
     input {
       border-radius: 6px;
       border: 0;
-      background: ${props => props.theme["gray-900"]};
-      color: ${props => props.theme["gray-300"]};
+      background: ${(props) => props.theme["gray-900"]};
+      color: ${(props) => props.theme["gray-300"]};
       padding: 1rem;
       &::placeholder {
-        color: ${props => props.theme["gray-500"]};
+        color: ${(props) => props.theme["gray-500"]};
       }
     }
 
     button[type="submit"] {
       height: 50px;
       border: 0;
-      background: ${props => props.theme["green-500"]};
-      color: ${props => props.theme.white};
+      background: ${(props) => props.theme["green-500"]};
+      color: ${(props) => props.theme.white};
       font-weight: bold;
       padding: 0 1.25rem;
       border-radius: 6px;
@@ -49,7 +49,7 @@ export const Content = styled(Dialog.Content)`
       cursor: pointer;
 
       &:hover {
-        background: ${props => props.theme["green-700"]};
+        background: ${(props) => props.theme["green-700"]};
         transition: background-color 0.2s;
       }
     }
@@ -64,7 +64,7 @@ export const CloseButton = styled(Dialog.Close)`
   right: 1.5rem;
   line-height: 0;
   cursor: pointer;
-  color: ${props => props.theme["gray-500"]};
+  color: ${(props) => props.theme["gray-500"]};
 `;
 
 export const TransactionType = styled(RadioGroup.Root)`
@@ -75,11 +75,13 @@ export const TransactionType = styled(RadioGroup.Root)`
 `;
 
 interface TransactionTypeButtonProps {
-  variant: 'income' | 'outcome';
+  variant: "income" | "outcome";
 }
 
-export const TransactionTypeButton = styled(RadioGroup.Item) <TransactionTypeButtonProps>`
-  background: ${props => props.theme["gray-700"]};
+export const TransactionTypeButton = styled(
+  RadioGroup.Item
+)<TransactionTypeButtonProps>`
+  background: ${(props) => props.theme["gray-700"]};
   padding: 1rem;
   display: flex;
   align-items: center;
@@ -88,30 +90,38 @@ export const TransactionTypeButton = styled(RadioGroup.Item) <TransactionTypeBut
   border-radius: 6px;
   cursor: pointer;
   border: 0;
-  color: ${props => props.theme["gray-300"]};
-  
+  color: ${(props) => props.theme["gray-300"]};
+
   svg {
-    color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]};
+    color: ${(props) =>
+      props.variant === "income"
+        ? props.theme["green-300"]
+        : props.theme["red-300"]};
   }
 
-  &[data-state='unchecked']:hover {
+  &[data-state="unchecked"]:hover {
     transition: background-color 0.2s;
-    background: ${props => props.theme["gray-600"]};
+    background: ${(props) => props.theme["gray-600"]};
   }
 
-  &[data-state='checked'] {
-    color: ${props => props.theme.white};
-    background: ${props => props.variant === 'income' ? props.theme["green-500"] : props.theme["red-500"]};
+  &[data-state="checked"] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.variant === "income"
+        ? props.theme["green-500"]
+        : props.theme["red-500"]};
 
     svg {
-      color: ${props => props.theme.white};
+      color: ${(props) => props.theme.white};
     }
   }
 
-  ${props => props.variant === "outcome" && css`
-    &:focus {
-      outline: 0;
-      box-shadow: 0 0 0 2px ${props => props.theme['red-500']};
-    }
-  `}
+  ${(props) =>
+    props.variant === "outcome" &&
+    css`
+      &:focus {
+        outline: 0;
+        box-shadow: 0 0 0 2px ${(props) => props.theme["red-500"]};
+      }
+    `}
 `;
--- projeto-03/dt-money/src/pages/Transactions/index.tsx
+++ projeto-03/dt-money/src/pages/Transactions/index.tsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
+
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+
 import * as S from "./styles";
 
 export function Transactions() {
+  const { transactions } = useContext(TransactionsContext);
+
   return (
     <div>
       <Header />
@@ -14,24 +20,20 @@ export function Transactions() {
         <SearchForm />
         <S.TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <S.PriceHighlight variant="income">
-                  R$ 12.000,00
-                </S.PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width="50%">Hambúrguer</td>
-              <td>
-                <S.PriceHighlight variant="outcome">-R$ 59,00</S.PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <S.PriceHighlight variant={transaction.type}>
+                      {transaction.price}
+                    </S.PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </S.TransactionsTable>
       </S.TransactionsContainer>
