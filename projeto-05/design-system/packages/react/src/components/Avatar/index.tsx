77da87592dc3ea75f29ec48595a60c1d9c89ff5b
Felipe Mamede <felipebrasilmv@gmail.com>
2023-10-10 23:11:38 -0300

    feat: addon de acessibilidade

---
+++ projeto-05/design-system/packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,19 @@
+import { User } from 'phosphor-react'
+import { ComponentProps } from 'react'
+import * as S from './styles'
+
+export type AvatarProps = ComponentProps<typeof S.AvatarImage>
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <S.AvatarContainer>
+      <S.AvatarImage {...props} />
+
+      <S.AvatarFallback delayMs={600}>
+        <User />
+      </S.AvatarFallback>
+    </S.AvatarContainer>
+  )
+}
+
+Avatar.displayName = 'Avatar'
