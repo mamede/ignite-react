2a2e78e251226cf6ea374f7ba5b040595da0f7cb
Felipe Mamede <felipebrasilmv@gmail.com>
2023-10-14 14:16:45 -0300

    :tada: initial commit

---
+++ projeto-04/desafio-04/ignite-shop/src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from "../styles/pages/app"
+
+import Image from "next/image"
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} alt="" width={130} height={52} />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
