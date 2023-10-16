e259928fd35aee48828d5b47822c2a93fa681020
Felipe Mamede <felipebrasilmv@gmail.com>
2023-10-16 13:00:10 -0300

    feat: componente claim username form

---
+++ projeto-06/ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,15 @@
+import { Button, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import * as S from './styles'
+
+export function ClaimUsernameForm() {
+  return (
+    <S.Form as="form">
+      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
+      <Button size="sm" type="submit">
+        Reservar
+        <ArrowRight />
+      </Button>
+    </S.Form>
+  )
+}
